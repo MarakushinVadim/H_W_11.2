cbcd3a63e3e1ea8c008237148c83cdf4c0e00de7
Marakushin Vadim <marakushin.vadim@gmail.com>
2023-12-24 15:44:02 +0300

    add gitignore

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def up_func(word):
+  '''Преобразование текста в заглавные буквы'''
   return word.upper()
+
+
+def cap_func(word):
+  '''функция для преобразования первой буквы слова в заглавную'''
+  return word.capitalize()
